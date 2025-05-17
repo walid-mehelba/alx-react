41a8f1c2f505b27fc24a0733dc220ef5eaf3fb01
walid mehelba <walid_mehelba@hotmail.com>
2025-05-17 04:27:45 +0300

    task_2

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -10,15 +10,34 @@ function App() {
         <img src={logo} alt="ALX logo" />
         <h1>School dashboard</h1>
       </div>
+
       <div className="App-body">
         <p>Login to access the full dashboard</p>
+
+        <form className="App-login-form">
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" />
+          </div>
+
+          <button type="submit">OK</button>
+        </form>
       </div>
+
       <div className="App-footer">
-        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
       </div>
     </>
   );
 }
 
 export default App;
+
 
--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -5,3 +5,7 @@ export function getFullYear() {
 export function getFooterCopy(isIndex) {
   return isIndex ? 'ALX' : 'ALX main dashboard';
 }
+
+export function getLatestNotification() {
+  return '<strong>Urgent requirement</strong> - complete by EOD';
+}
