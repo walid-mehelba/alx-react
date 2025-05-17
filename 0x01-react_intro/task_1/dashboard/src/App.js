6af5e08f87e00cf8671444b78f280c0f184b42f9
walid mehelba <walid_mehelba@hotmail.com>
2025-05-17 03:25:26 +0300

    task_1

--- 0x01-react_intro/task_1/dashboard/src/App.js
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './alx_logo.png';
 import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
 
 function App() {
   return (
@@ -13,10 +14,11 @@ function App() {
         <p>Login to access the full dashboard</p>
       </div>
       <div className="App-footer">
-        <p>Copyright 2020 - ALX</p>
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
       </div>
     </>
   );
 }
 
 export default App;
+
---
+++ 0x01-react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,7 @@
+export function getFullYear() {
+  return new Date().getFullYear();
+}
+
+export function getFooterCopy(isIndex) {
+  return isIndex ? 'ALX' : 'ALX main dashboard';
+}
